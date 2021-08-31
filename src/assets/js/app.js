d740103673a42661ac04d171428388f7fb6fb619
Евгений Клюев <craftycoderpro@gmail.com>
2021-08-31 18:38:05 +0300

    add custom select

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,15 +1,27 @@
 // node modules:
 import Glide from '@glidejs/glide';
-import Accordion from 'accordion-js';
-import Inputmask from "inputmask";
+// import Accordion from 'accordion-js';
+// import Inputmask from "inputmask";
 import customSelect from 'custom-select';
-import LazyLoad from 'lazyload';
+// import LazyLoad from 'lazyload';
+
+
 
 
 
 document.addEventListener('DOMContentLoaded', () => {
   // https://github.com/custom-select/custom-select
-  customSelect("select");
+
+  const range = document.querySelector('input[type="range"]');
+  const rangeBg = document.querySelector('#range');
+  const rangeOutput = document.querySelector('.choose__hours');
+  const price = document.querySelector('.choose__price');
+
+  range.addEventListener('input', function() {
+    rangeBg.style.width = `${(this.value - 1) * 25}%`;
+    rangeOutput.innerText = `${this.value} hours`;
+    price.innerText = `$${this.value * 105 + 5}`;
+  });
 
   // ----------------------------------------------------------- slider:
   const options = {
@@ -29,5 +41,26 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   };
 
-  new Glide('.glide', options).mount();
+  // new Glide('.glide', options).mount();
+
+
+  customSelect('#cattering-select');
+
+  const catteringSelect = document.querySelector('#cattering-select');
+  const catteringSelectSpan = document.querySelector('.choose .custom-select-opener span');
+  const chooseParagraph = document.querySelector('.choose__paragraph');
+
+  catteringSelect.addEventListener('change', function() {
+    catteringSelectSpan.setAttribute('data-value', this.value);
+    catteringSelectSpan.innerText = '';
+    chooseParagraph.innerText = this.options[this.selectedIndex].innerText;
+    chooseParagraph.style = '';
+    setTimeout(() => chooseParagraph.style = 'animation: clr 0.3s ease-out forwards')
+  });
+
+  const mediaQueryList = window.matchMedia(`screen and (min-width: 768px)`);
+  if (mediaQueryList.matches) {
+    document.querySelectorAll('.choose .custom-select-option')[1].click();
+  }
+
 })
